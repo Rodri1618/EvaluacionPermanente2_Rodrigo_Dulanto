2526291cc94071a1fffb4a2861d658efc7d68c34
rodrigo <rodrigodulantotejeda@gmail.com>
2025-02-12 22:13:55 -0500

    Initial Commit

---
+++ ep02/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ep02
+//
+//  Created by Casa on 12/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ep02/ep02App.swift
@@ -0,0 +1,17 @@
+//
+//  ep02App.swift
+//  ep02
+//
+//  Created by Casa on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ep02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
